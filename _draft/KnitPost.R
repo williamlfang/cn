6097e22f6dfbc4606de45bf4c4992defd5ace293
williamlfang <william.lian.fang@gmail.com>
2014-01-24 02:23:16 +0800

    updates

--- _draft/KnitPost.R
+++ _draft/KnitPost.R
@@ -3,7 +3,6 @@ KnitPost <- function(input, base.url = "/") {
   opts_knit$set(base.url = base.url)
   fig.path <- paste0("figs/", sub(".Rmd$", "", basename(input)), "/")
   opts_chunk$set(fig.path = fig.path)
-  opts_chunk$set(fig.cap = "center")
   render_jekyll()
   knit(input, envir = parent.frame())
 }

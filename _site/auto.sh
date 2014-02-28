2a4a65e9340913929bafb77b6e7619149fb3ee8d
williamlfang <william.lian.fang@gmail.com>
2014-02-28 12:05:17 +0800

    updates

--- _site/auto.sh
+++ _site/auto.sh
@@ -1,9 +1,9 @@
-cd /home/william/en
+cd /home/william/cn
 git add . -A
 git commit -m "updates"
 
 git remote rm origin
-git remote add origin git@github.com:williamlfang/en.git
+git remote add origin git@github.com:williamlfang/cn.git
 git push origin gh-pages
 
 jekyll --server

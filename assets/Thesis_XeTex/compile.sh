ca3e0d41219fc29c66406f8d47f053a1922f68fb
williamlfang <william.lian.fang@gmail.com>
2014-05-12 14:16:52 +0800

    2014-05-12

--- assets/Thesis_XeTex/compile.sh
+++ assets/Thesis_XeTex/compile.sh
@@ -4,3 +4,12 @@ bibtex main.aux
 xelatex -no-pdf -quiet -time-statistics main.tex
 makeindex main.nlo -s nomencl.ist -o main.nls
 xelatex -quiet -time-statistics main.tex
+
+#+-----------------+
+#|     cleanup     |
+#+-----------------+
+# clean:  
+	rm -f core \#* *~ *.blg *.dvi *.log *.ps *.tmp *.out *.gz 
+# t#exclean: clean 
+	rm -f *.aux *.bbl *.lof *.lot *.toc *.xdv  *.idv  *.eoc *.nls *.nlo *.ilg
+# realclean:  texclean clean
--- auto.sh
+++ auto.sh
@@ -1,6 +1,6 @@
 cd /home/william/cn
 git add . -A
-git commit -m "2014-05-10"
+git commit -m "2014-05-12"
 
 git remote rm origin
 git remote add origin git@github.com:williamlfang/cn.git

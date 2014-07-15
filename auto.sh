24579437f0ae13a55ad907e8c9632b7f0b999354
williamlfang <william.lian.fang@gmail.com>
2014-07-15 11:54:00 +0800

    Added notes for Tue Jul 15 11:54:00 CST 2014

--- auto.sh
+++ auto.sh
@@ -1,6 +1,6 @@
 cd /home/william/cn
 git add . -A
-git commit -m "2014-05-15"
+git commit -m "Added notes for $(date)"
 
 git remote rm origin
 git remote add origin git@github.com:williamlfang/cn.git

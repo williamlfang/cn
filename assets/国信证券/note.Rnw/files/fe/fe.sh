222bbf2a25b4050ae8f371504b2d28324520acaa
williamlfang <william.lian.fang@gmail.com>
2014-07-15 14:51:54 +0800

    Added notes for Tue Jul 15 14:51:54 CST 2014

---
+++ assets/国信证券/note.Rnw/files/fe/fe.sh
@@ -0,0 +1,28 @@
+Rscript -e "library(knitr); knit('fe.Rnw')"
+
+Rscript -e  "library(knitr); knit('chapters/chapter01.Rnw')"
+Rscript -e  "library(knitr); knit('chapters/chapter02.Rnw')"
+
+
+
+
+xelatex -no-pdf -quiet -time-statistics fe.tex
+bibtex fe.bib
+bibtex fe.aux
+xelatex -no-pdf -quiet -time-statistics fe.tex
+makeindex main.nlo -s nomencl.ist -o fe.nls
+xelatex -quiet -time-statistics fe.tex
+
+
+#+-----------------+
+#|     cleanup     |
+#+-----------------+
+# clean:  
+	rm -f core \#* *~ *.blg *.dvi *.log *.ps *.tmp *.out *.gz 
+# texclean: clean 
+	rm -f *.aux *.bbl *.lof *.lot *.toc *.xdv  *.idv  *.tex
+	rm -f org.idx people.idx 
+# realclean:  texclean clean
+
+evince fe.pdf
+

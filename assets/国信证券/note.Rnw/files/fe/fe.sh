7a829aa51f2b076d844e33afaf53be3880ce16c9
williamlfang <william.lian.fang@gmail.com>
2014-07-18 12:05:29 +0800

    Added notes for Fri Jul 18 12:05:29 CST 2014

--- assets/国信证券/note.Rnw/files/fe/fe.sh
+++ assets/国信证券/note.Rnw/files/fe/fe.sh
@@ -1,10 +1,20 @@
+#+-----------------+
+#|     cleanup     |
+#+-----------------+
+# clean:  
+	rm -f core \#* *~ *.blg *.dvi *.log *.ps *.tmp *.out *.gz 
+# texclean: clean 
+	rm -f *.aux *.bbl *.lof *.lot *.toc *.xdv  *.idv *.tex
+	rm -f org.idx people.idx 
+# realclean:  texclean clean
+
+
+
 Rscript -e "library(knitr); knit('fe.Rnw')"
 
 Rscript -e  "library(knitr); knit('chapters/chapter01.Rnw')"
 Rscript -e  "library(knitr); knit('chapters/chapter02.Rnw')"
-
-
-
+Rscript -e  "library(knitr); knit('chapters/explory-data.Rnw')"
 
 xelatex -no-pdf -quiet -time-statistics fe.tex
 bibtex fe.bib
@@ -20,9 +30,12 @@ xelatex -quiet -time-statistics fe.tex
 # clean:  
 	rm -f core \#* *~ *.blg *.dvi *.log *.ps *.tmp *.out *.gz 
 # texclean: clean 
-	rm -f *.aux *.bbl *.lof *.lot *.toc *.xdv  *.idv  *.tex
+	rm -f *.aux *.bbl *.lof *.lot *.toc *.xdv  *.idv *.tex
 	rm -f org.idx people.idx 
+	rm -r figure    ## remove figure 
+	rm -r cache		## remove cache
 # realclean:  texclean clean
+
 
 evince fe.pdf
 
